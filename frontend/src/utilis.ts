270f561dd32ccce35edb01a79486b1f227a9bddf
Umesh Tiruvalluru <tiruvalluruumesh@gmail.com>
2024-12-12 11:46:09 +0530

    Build errors resolved

--- frontend/src/utilis.ts
+++ frontend/src/utilis.ts
@@ -7,15 +7,3 @@ export function formatDate(date: Date): string {
     minute: "numeric",
   }).format(date);
 }
-
-
- function correctedLink(link: string): string {
-    if (link.includes("youtu.be")) {
-      return link.replace("youtu.be/", "youtube.com/embed/");
-    } else if (link.includes("youtube.com/watch")) {
-      return link.replace("youtube.com/watch?v=", "youtube.com/embed/");
-    }
-    return link;
-  }
-
-
